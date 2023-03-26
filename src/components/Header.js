796cecfd20b5aa33dfee1cb04b84f62faf156de4
ErickDC <erickdiazc717@gmail.com>
2023-03-26 12:31:53 -0500

    header updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,21 @@
 import React from 'react';
+// Images
+import Logo from '../assets/logo.svg'
 
 const Header = () => {
-  return <div className="bg-pink-300">Header</div>;
+  return <header className="py-8">
+    <div className="container mx-auto">
+      <div className="flex justify-between items-center">
+        {/* Logo */}
+        <a href="#">
+          <img src={Logo} alt="" />
+        </a>
+
+        {/* Button */}
+        <button className="btn btn-sm">Work with me</button>
+      </div>
+    </div>
+  </header>;
 };
 
 export default Header;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -15,6 +15,7 @@ const Nav = () => {
           activeClass="active"
           smooth={true}
           spy={true}
+          offset={-200}
           className="cursor-pointer w-[60px] h-[60px] flex items-center justify-center"
         >
           <BiHomeAlt />
